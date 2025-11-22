b04519fc65b0634169d5a127ef8c55b5ee0a7450
Gabriel Salviete <gsalviete@gmail.com>
2025-11-22 16:33:03 -0300

    chore: validation schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose'
-import * as Joi from 'joi';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { envValidationSchema } from './config/env.validation';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ 
+      isGlobal: true,
+      validationSchema: envValidationSchema }),
     MongooseModule.forRootAsync({
       useFactory: () => ({
        uri: process.env.MONGODB_URI,
