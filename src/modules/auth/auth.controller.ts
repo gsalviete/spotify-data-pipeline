553a3a95e01ab2ff4e01a4a780d6b300ad223ec5
Gabriel Salviete <gsalviete@gmail.com>
2026-02-01 00:10:51 -0300

    login and session

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -11,6 +11,12 @@ export class AuthController {
   @Redirect()
   async login(@Session() session: Record<string, any>) {
     const url = await this.authService.login(session);
+    /*await new Promise<void>((resolve, reject) => {
+      session.save((err: any) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });*/
     return { url, statusCode: 302 };
   }
 
--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Session } from '@nestjs/common';
+import { Controller, Get, UseGuards, Session } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-import { CreateDashboardDto } from './dto/create-dashboard.dto';
-import { UpdateDashboardDto } from './dto/update-dashboard.dto';
 import { SessionAuthGuard } from 'src/common/guards/session-auth.guard';
 
 @Controller('dashboard')
