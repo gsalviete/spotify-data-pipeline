e99e70dd5b52f23886a0ec9eb9363da7e21617f4
Gabriel Salviete <gsalviete@gmail.com>
2026-01-31 23:34:05 -0300

    controller

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Session } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { CreateDashboardDto } from './dto/create-dashboard.dto';
 import { UpdateDashboardDto } from './dto/update-dashboard.dto';
+import { SessionAuthGuard } from 'src/common/guards/session-auth.guard';
 
 @Controller('dashboard')
 export class DashboardController {
   constructor(private readonly dashboardService: DashboardService) {}
 
   @Get('/top/artists')
+  @UseGuards(SessionAuthGuard)
+  async getTopArtists(@Session() session: Record<string, any>){
+    const userId = session.userId;
+    return this.dashboardService.getTopArtists(userId);
+  }
   
 }
