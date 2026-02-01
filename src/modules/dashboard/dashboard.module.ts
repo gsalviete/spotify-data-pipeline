17f8dddb0f85318a2b1dcbc42385b3b00593cb07
Gabriel Salviete <gsalviete@gmail.com>
2026-01-31 23:37:11 -0300

    fix: import modules

--- src/modules/dashboard/dashboard.module.ts
+++ src/modules/dashboard/dashboard.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { HttpModule } from '@nestjs/axios';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [HttpModule, UserModule],
   controllers: [DashboardController],
   providers: [DashboardService],
 })
