71715d16502f8172bd4a62ec883576a8f0a05cd1
Gabriel Salviete <gsalviete@gmail.com>
2026-02-08 00:58:28 -0300

    change folders

---
+++ backend/src/config/env.validation.ts
@@ -0,0 +1,10 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('dev', 'prod', 'test').default('dev'),
+  PORT: Joi.number().default(3000),
+  MONGODB_URI: Joi.string().uri().required(),
+  SPOTIFY_CLIENT_ID: Joi.string().required(),
+  SPOTIFY_CLIENT_SECRET: Joi.string().required(),
+  SPOTIFY_REDIRECT_URI: Joi.string().uri().required(),
+});
