a3e26fcad0d91c6bd674e0e3e2bfc5a63fdec327
Gabriel Salviete <gsalviete@gmail.com>
2026-02-14 20:28:01 -0300

    linter on guards

--- backend/src/common/guards/session-auth.guard.ts
+++ backend/src/common/guards/session-auth.guard.ts
@@ -1,14 +1,21 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
-import { Observable } from "rxjs";
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class SessionAuthGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const req = context.switchToHttp().getRequest();
-        
-        if (!req.session.userId){
-            throw new UnauthorizedException();
-        }
-       return true;
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    if (!req.session.userId) {
+      throw new UnauthorizedException();
     }
+    return true;
+  }
 }
