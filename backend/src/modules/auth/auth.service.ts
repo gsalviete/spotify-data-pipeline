dcb80513c5e78eae328a1374e43e48fb53a9aeb6
Gabriel Salviete <gsalviete@gmail.com>
2026-02-16 17:59:48 -0300

    feat: refreshAccessToken

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -10,13 +10,13 @@ export class AuthService {
   constructor(
     private readonly configService: ConfigService,
     private readonly httpService: HttpService,
-    private readonly userService: UserService
+    private readonly userService: UserService,
   ) {}
 
   async login(session: Record<string, any>) {
     const state = crypto.randomUUID();
     session.oauthState = state;
-    
+
     const clientId = this.configService.get<string>('SPOTIFY_CLIENT_ID');
     const redirectUri = this.configService.get<string>('SPOTIFY_REDIRECT_URI');
 
@@ -35,13 +35,16 @@ export class AuthService {
     return `https://accounts.spotify.com/authorize?${params.toString()}`;
   }
 
-  async callback(session: Record<string, any>, dto: CallbackDto) {  
+  async callback(session: Record<string, any>, dto: CallbackDto) {
     if (dto.state != session.oauthState) {
       throw new NotAcceptableException('State not matches with Code');
     }
-    
+
     const tokens = await this.exchangeCodeForToken(dto);
-    const user = await this.loginWithSpotify(tokens.access_token, tokens.refresh_token);
+    const user = await this.loginWithSpotify(
+      tokens.access_token,
+      tokens.refresh_token,
+    );
 
     session.userId = user.spotifyId;
 
@@ -49,15 +52,20 @@ export class AuthService {
   }
 
   async exchangeCodeForToken(dto: CallbackDto) {
-    const redirectUri = this.configService.getOrThrow<string>('SPOTIFY_REDIRECT_URI');
+    const redirectUri = this.configService.getOrThrow<string>(
+      'SPOTIFY_REDIRECT_URI',
+    );
     const data = new URLSearchParams({
       grant_type: 'authorization_code',
       code: dto.code,
-      redirect_uri: redirectUri
+      redirect_uri: redirectUri,
     });
 
-    const spotifySecret = this.configService.getOrThrow<string>('SPOTIFY_CLIENT_SECRET');
-    const spotifyId = this.configService.getOrThrow<string>('SPOTIFY_CLIENT_ID')
+    const spotifySecret = this.configService.getOrThrow<string>(
+      'SPOTIFY_CLIENT_SECRET',
+    );
+    const spotifyId =
+      this.configService.getOrThrow<string>('SPOTIFY_CLIENT_ID');
     const response = await firstValueFrom(
       this.httpService.post(
         'https://accounts.spotify.com/api/token',
@@ -67,9 +75,7 @@ export class AuthService {
             'Content-Type': 'application/x-www-form-urlencoded',
             Authorization:
               'Basic ' +
-              Buffer.from(
-                `${spotifyId}:${spotifySecret}`,
-              ).toString('base64'),
+              Buffer.from(`${spotifyId}:${spotifySecret}`).toString('base64'),
           },
         },
       ),
@@ -79,16 +85,15 @@ export class AuthService {
 
   async getProfile(accessToken: string) {
     const response = await firstValueFrom(
-      this.httpService.get(
-      'https://api.spotify.com/v1/me',
-   {
-    headers: {
-      Authorization: 'Bearer ' + accessToken
-    }
-  }))
+      this.httpService.get('https://api.spotify.com/v1/me', {
+        headers: {
+          Authorization: 'Bearer ' + accessToken,
+        },
+      }),
+    );
 
-  const data = await response.data;
-  return data; 
+    const data = await response.data;
+    return data;
   }
 
   async loginWithSpotify(accessToken: string, refreshToken: string) {
@@ -104,5 +109,38 @@ export class AuthService {
     });
 
     return user;
+  }
+
+  async refreshAccessToken(spotifyId: string) {
+    const spotifySecret = this.configService.getOrThrow<string>(
+      'SPOTIFY_CLIENT_SECRET',
+    );
+    const clientId = this.configService.getOrThrow<string>('SPOTIFY_CLIENT_ID');
+    const user = await this.userService.findUser(spotifyId);
+
+    const data = new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: user.refreshToken,
+    });
+    const response = await firstValueFrom(
+      this.httpService.post(
+        'https://accounts.spotify.com/api/token',
+        data.toString(),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization:
+              'Basic ' +
+              Buffer.from(`${clientId}:${spotifySecret}`).toString('base64'),
+          },
+        },
+      ),
+    );
+    await this.userService.updateTokens(spotifyId, {
+      accessToken: response.data.access_token,
+      refreshToken: response.data.refresh_token ?? user.refreshToken,
+    });
+
+    return response.data;
   }
 }
